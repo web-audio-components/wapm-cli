7ef2abce5e20084de704fc77abfe07a345189d8d
Nick Thompson <nct25@cornell.edu>
2012-11-29 13:49:33 -0500

    thorough package testing

--- test/package.test.js
+++ test/package.test.js
@@ -4,22 +4,52 @@ process.env.NODE_ENV = 'test';
 
 // Dependencies
 var 
-  chai = require( 'chai' ),
-  expect = chai.expect,
-  should = chai.should(),
-  mockLoad = require( './lib/mockload' ),
-  request = require( './lib/mocks' ).request,
-  Package = mockLoad( '../lib/package.js', { request: request } ).Package;
+  chai      = require( 'chai' ),
+  expect    = chai.expect,
+  should    = chai.should(),
+  fs        = require( 'fs' ),
+  path      = require( 'path' ),
+  rimraf    = require( 'rimraf' ),
+  config    = require( '../lib/config' ),
+  mockLoad  = require( './lib/mockload' ),
+  request   = require( './lib/mocks' ).request,
+  Package   = mockLoad( '../lib/package.js', { request: request } ).Package;
 
 describe( 'Package tests.', function () {
+  var dest = path.resolve( path.resolve( __dirname, '../' ), config.dest );
+
+  afterEach( function ( done ) {
+    if ( fs.existsSync ( dest ) ) {
+      rimraf( dest, function ( err ) {
+        done( err );
+      });
+    } else {
+      done();
+    }
+  });
 
   it( 'Fetch info', function () {
-    var pkg = new Package( 'simple-gain', {} );
+    var pkg = new Package( 'module-one', {} );
     pkg.info( function ( err, data ) {
       if ( err ) { throw err; }
       expect( data ).to.be.a( 'object' );
       expect( data ).to.have.property( 'name' );
     });
+  });
+
+
+  it( 'Install single', function () {
+    var pkg = new Package( 'module-one', {} );
+    pkg.install();
+    expect( fs.existsSync( dest + '/module-one.js' ) ).to.equal(true);
+  });
+
+  it( 'Install with dependency chain', function () {
+    var pkg = new Package( 'module-three', {} );
+    pkg.install();
+    expect( fs.existsSync( dest + '/module-one.js' ) ).to.equal(true);
+    expect( fs.existsSync( dest + '/module-two.js' ) ).to.equal(true);
+    expect( fs.existsSync( dest + '/module-three.js' ) ).to.equal(true);
   });
 
 });
